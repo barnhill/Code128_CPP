bfb19e8c3a110300ddf5c0d9680204261db5d668
Brad Barnhill <bradbarnhill@hotmail.com>
2016-01-30 12:01:49 -0600

    add copyrights to C128 code files

--- Code128_CPlusPlus/C128_Defines.h
+++ Code128_CPlusPlus/C128_Defines.h
@@ -1,3 +1,28 @@
+/**
+ * @file C128_Defines.h
+ * @brief The Code128 encoding library static definitions
+ *
+ * C128 - Code 128 library
+ * Copyright (C) 2015 Brad Barnhill
+ * mail: bradbarnhill@hotmail.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+ 
 #define NUL "\0"
 #define SOH "\x01"
 #define STX "\x02"
